25686da65fb43a906217e35ca5866a90ee9d0673
Vahram Nersesyan <vahnersesyan@gmail.com>
2025-12-01 00:49:20 +0100

    Implement UserService layer

--- domain/src/main/java/de/seuhd/campuscoffee/domain/impl/UserServiceImpl.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/impl/UserServiceImpl.java
@@ -1,13 +1,77 @@
 package de.seuhd.campuscoffee.domain.impl;
 
+import de.seuhd.campuscoffee.domain.exceptions.DuplicationException;
+import de.seuhd.campuscoffee.domain.model.Pos;
+import de.seuhd.campuscoffee.domain.model.User;
+import de.seuhd.campuscoffee.domain.ports.PosDataService;
+import de.seuhd.campuscoffee.domain.ports.UserDataService;
 import de.seuhd.campuscoffee.domain.ports.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     // TODO: Implement user service
+    private final UserDataService userDataService;
+
+    @Override
+    public @NonNull List<User> getAll() {
+        log.debug("Retrieving all Users");
+        return userDataService.getAll();
+    }
+
+    @Override
+    public @NonNull User getById(@NonNull Long id) {
+        log.debug("Retrieving User with ID: {}", id);
+        return userDataService.getById(id);
+    }
+
+    @Override
+    public @NonNull User getByloginName(@NonNull String loginName) {
+        log.debug("Retrieving User with name: {}", loginName);
+        return userDataService.getByLoginName(loginName);
+    }
+
+    @Override
+    public @NonNull User upsert(@NonNull User user) {
+        if (user.id() == null) {
+            // create a new User
+            log.info("Creating new User: {}", user.loginName());
+        } else {
+            // update an existing User
+            log.info("Updating User with ID: {}", user.id());
+            // User ID must be set
+            Objects.requireNonNull(user.id());
+            // User must exist in the database before the update
+            userDataService.getById(user.id());
+        }
+        return performUpsert(user);
+    }
+
+    @Override
+    public void delete(@NonNull Long id) {
+        log.info("Trying to delete User with ID: {}", id);
+        userDataService.delete(id);
+        log.info("Deleted User with ID: {}", id);
+    }
+
+    private @NonNull User performUpsert(@NonNull User user) {
+        try {
+            User upsertedUser = userDataService.upsert(user);
+            log.info("Successfully upserted User with ID: {}", upsertedUser.id());
+            return upsertedUser;
+        } catch (DuplicationException e) {
+            log.error("Error upserting User '{}': {}", user.loginName(), e.getMessage());
+            throw e;
+        }
+    }
 }
+
+
--- domain/src/main/java/de/seuhd/campuscoffee/domain/ports/UserService.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/ports/UserService.java
@@ -1,5 +1,16 @@
 package de.seuhd.campuscoffee.domain.ports;
 
+import de.seuhd.campuscoffee.domain.model.Pos;
+import de.seuhd.campuscoffee.domain.model.User;
+import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+
 public interface UserService {
     //TODO: Define user service interface
+    @NonNull List<User> getAll();
+    @NonNull User getById(@NonNull Long id);
+    @NonNull User getByloginName(@NonNull String loginName);
+    @NonNull User upsert(@NonNull User user);
+    void delete(@NonNull Long id);
 }
