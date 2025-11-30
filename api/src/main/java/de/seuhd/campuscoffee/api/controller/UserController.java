20d83be81232c2f21352a66ccdb00d1214a548ab
Vahram Nersesyan <vahnersesyan@gmail.com>
2025-11-30 17:11:13 +0100

    Add UserController without implementations of missing interfaces

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/UserController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/UserController.java
@@ -1,10 +1,21 @@
 package de.seuhd.campuscoffee.api.controller;
 
+import de.seuhd.campuscoffee.api.dtos.PosDto;
+import de.seuhd.campuscoffee.api.dtos.UserDto;
+import de.seuhd.campuscoffee.api.mapper.UserDtoMapper;
+import de.seuhd.campuscoffee.domain.ports.PosService;
+import de.seuhd.campuscoffee.domain.ports.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static de.seuhd.campuscoffee.api.util.ControllerUtils.getLocation;
 
 @Tag(name = "Users", description = "Operations related to user management.")
 @Controller
@@ -13,4 +24,79 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     //TODO: Implement user controller
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    @GetMapping("")
+    public ResponseEntity<List<UserDto>> getAll() {
+
+        return ResponseEntity.ok(
+                userService.getAll().stream()
+                        .map(userDtoMapper::fromDomain)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                userDtoMapper.fromDomain(userService.getById(id))
+        );
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<UserDto> filter(
+            @RequestParam("loginName") String loginName) {
+
+        return ResponseEntity.ok(
+                userDtoMapper.fromDomain(userService.getByloginName(loginName))
+        );
+    }
+
+    @PostMapping("")
+    public ResponseEntity<UserDto> create(
+            @RequestBody @Valid UserDto userDto) {
+
+        UserDto created = upsert(userDto);
+        return ResponseEntity
+                .created(getLocation(created.id()))
+                .body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserDto userDto) {
+
+        if (!id.equals(userDto.id())) {
+            throw new IllegalArgumentException("User ID in path and body do not match.");
+        }
+        return ResponseEntity.ok(
+                upsert(userDto)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id) {
+        userService.delete(id); // throws NotFoundException if no POS with the provided ID exists
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Common upsert logic for create and update.
+     *
+     * @param userDto the User DTO to map and upsert
+     * @return the upserted User mapped back to the User format.
+     */
+    private UserDto upsert(UserDto userDto) {
+        return userDtoMapper.fromDomain(
+                userService.upsert(
+                        userDtoMapper.toDomain(userDto)
+                )
+        );
+    }
+
 }
